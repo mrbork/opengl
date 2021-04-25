09ae0b4d880ca3acbc9176bf18faf18291100c7c
Bork <Bork@DESKTOP-0IMGBE6>
2021-04-25 16:44:01 -0700

    Test commit

--- src/main.cpp
+++ src/main.cpp
@@ -48,14 +48,17 @@ int main(void)
     GLFWwindow* window;
 
     /* Initialize the library */
-    if ( !glfwInit() )
-        return -1;
+    if ( !glfwInit( ) ) {
+        printf( "Failed to initialize GLFW." );
+        glfwTerminate( );
+        return 1;
+    }
 
     /* Create a windowed mode window and its OpenGL context */
     window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
     if ( !window )
     {
-        glfwTerminate();
+        glfwTerminate( );
         return -1;
     }
 
